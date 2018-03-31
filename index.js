1b3c31281ed1df900304b74d12c2a8375eeeef6a
Vincenzo Chianese <vincenz.chianese@icloud.com>
2018-03-31 03:35:38 +0200

    Handle default endpoint

--- index.js
+++ index.js
@@ -1,8 +1,36 @@
 const { config, Client } = require('kubernetes-client')
 const JSONStream = require('JSONStream')
+const axios = require('axios').default.create({ baseURL: 'http://localhost:9876' })
 const debug = require('debug')('eg-ingress-controller')
 
 const client = new Client({ config: config.getInCluster() })
+
+const addIngressRule = (uid, spec) => {
+  const createServiceEndpoint = (name, { serviceName, servicePort }) => {
+    return axios.put(`/service-endpoints/${encodeURIComponent(serviceName)}`, {
+      url: `http://${serviceName}:${servicePort}`
+    })
+  }
+
+  const createApiEndpoint = (name, host, paths) => axios.put(`/api-endpoints/${encodeURIComponent(name)}`, { host, paths })
+
+  if (spec.rules) {
+    spec.rules.forEach((rule) => {
+    })
+  }
+
+  if (spec.backend) {
+    debug(`Creating default serviceEndpoint and ApiEndpoint for`, spec.backend)
+    createServiceEndpoint('default', spec.backend)
+      .then(() => createApiEndpoint('default', '*'))
+      .catch((err) => debug(err.message))
+  }
+}
+
+const deleteIngressRule = (uid) => {
+
+}
+
 
 client.loadSpec().then((kubeApi) => {
   debug('Kubernetes API Specs loaded')
@@ -14,17 +42,16 @@ client.loadSpec().then((kubeApi) => {
     const { spec } = ingressWatchEvent.object
     const { uid } = ingressWatchEvent.object.metadata
 
-    debug(ingressWatchEvent.type)
     switch (ingressWatchEvent.type) {
       case 'ADDED':
-        addIngressRule(uid, spec);
+        addIngressRule(uid, spec)
         break;
       case 'MODIFIED':
-        modifyIngressRule(uid, spec);
+        deleteIngressRule(uid)
+        addIngressRule(uid, spec)
         break;
       case 'DELETED':
-        deleteIngressRule(uid);
+        deleteIngressRule(uid)
     }
   })
-
 })
